245831771c716c988d1a4d77aadb68f8efb5de38
Kelson Palharini <kelsonpalharini@bsd.com.br>
2020-09-06 21:51:41 -0300

    added avatar update routes

---
+++ src/database/migrations/1599426866547-AddAvatarFieldToUsers.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export default class AddAvatarFieldToUsers1599426866547
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'users',
+      new TableColumn({
+        name: 'avatar',
+        type: 'varchar',
+        isNullable: true,
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('users', 'avatar');
+  }
+}
